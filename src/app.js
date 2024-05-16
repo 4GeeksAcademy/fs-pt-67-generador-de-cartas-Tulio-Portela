4a0d7bac653196cfc7f6c8a15241d6647925c16f
TulioPortela <156851372+Tulio-Portela@users.noreply.github.com>
2024-05-16 17:53:04 +0000

    concluido

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suits = ["♦", "♠", "♥", "♣"];
+
+  let randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomSuit = suits[Math.floor(Math.random() * suits.length)];
+
+  document.querySelector(".cardNumber").textContent = randomNumber;
+  let cardTops = document.querySelectorAll(".cardTop, .cardBottom");
+  cardTops.forEach(function(cardTop) {
+    cardTop.textContent = randomSuit;
+    if (randomSuit === "♦" || randomSuit === "♥") {
+      cardTop.classList.add("red");
+    }
+  });
 };
